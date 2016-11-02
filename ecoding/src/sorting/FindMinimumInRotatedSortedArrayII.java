6e190789f930bdbaa848f4ecafc52e47f48388a0
wynzn2009 <wangyuning0229@126.com>
2016-11-02 17:38:27 +0800

    FindMinimumInRotatedSortedArrayII

--- ecoding/src/sorting/FindMinimumInRotatedSortedArrayII.java
+++ ecoding/src/sorting/FindMinimumInRotatedSortedArrayII.java
@@ -1,12 +1,62 @@
 package sorting;
-//TODO
 public class FindMinimumInRotatedSortedArrayII {
 	public int findMin(int[] nums) {
-        return 0;
+		if(nums==null||nums.length==0){
+			return 0;
+		}
+		int temp = Integer.MAX_VALUE;
+		for(int i=0;i<nums.length;i++){
+			temp = Math.min(temp, nums[i]);
+		}
+        return temp;
+    }
+	public int findMin1(int[] nums) {
+		if(nums==null||nums.length==0){
+			return 0;
+		}
+		int temp = Integer.MAX_VALUE;
+		for(int i=0;i<nums.length;i++){
+			if(nums[i]<temp&&temp!=Integer.MAX_VALUE){
+				return nums[i];
+			}
+			temp = Math.min(temp, nums[i]);
+		}
+        return temp;
+    }
+	public static int findMin2(int[] nums) {
+		if(nums==null||nums.length==0){
+			return 0;
+		}
+		int l = 0;
+		int r = nums.length-1;
+		int m = (l+r)/2-l;
+		while((m!=l||m!=r)&&l<=r){
+			if(nums[l]<nums[r]){
+				return nums[l];
+			}
+			if(nums[m]>=nums[l]){
+				if(nums[m]==nums[l]&&nums[m]==nums[r]){
+					l++;
+					r--;
+					continue;
+				}
+				if(nums[m]<nums[r]){
+					return nums[m];
+				}else{
+					l=m+1;
+					m=(r-l)/2+l;
+				}
+			}else if(nums[m]<nums[l]){
+				r = m;
+				m=(r-l)/2+l;
+			}
+		}
+		return nums[m];
     }
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		int[] t = {10,1,10,10,10};
+		findMin2(t);
 	}
 
 }
---
+++ ecoding/src/sorting/ReverseString.java
@@ -0,0 +1,25 @@
+/**
+ * 
+ */
+package sorting;
+
+import org.apache.commons.lang3.StringUtils;
+
+/**
+ * @author wyn
+ * 344
+ *
+ */
+public class ReverseString {
+	public String reverseString(String s) {
+		return StringUtils.reverse(s);
+    }
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
